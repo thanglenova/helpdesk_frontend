52f80bec3793e7a95047e8c41da62c1539413cae
thanhhuyen1208 <lunachris1208@gmail.com>
2019-11-05 14:49:11 +0700

    skill functions and interface

--- src/app/core/services/skill.service.ts
+++ src/app/core/services/skill.service.ts
@@ -4,13 +4,14 @@ import {AlertService} from './alert.service';
 import {Observable, of} from 'rxjs';
 import {Skill} from 'src/app/shared/models/skill';
 import {catchError} from 'rxjs/operators';
+import {Category} from '../../shared/models/category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SkillService {
 
-  private serverUrl = 'https://helpdesk-kunlez-novahub.herokuapp.com/api/skills';    //Link API of the server
+  private serverUrl = 'https://helpdesk-kunlez-novahub.herokuapp.com/api/skills';    // Link API of the server
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -35,6 +36,41 @@ export class SkillService {
   getSkills(): Observable<Skill[]> {
     return this.http.get<Skill[]>(this.serverUrl).pipe(
       catchError(this.handleError<Skill[]>('getSkills'))
+    );
+  }
+
+  getSkillById(id: number): Observable<Skill> {
+    const url = `${this.serverUrl}/${id}`;
+    return this.http.get<Skill>(url).pipe(
+      catchError(this.handleError<Skill>('getSkillById'))
+    );
+  }
+
+  addSkill(skill: Skill): Observable<Skill> {
+    return this.http.post<Skill>(this.serverUrl, skill).pipe(
+      catchError(this.handleError<Skill>('addSkill'))
+    );
+  }
+
+  updateSkill(skill: Skill | number): Observable<Skill> {
+    const id = typeof skill === 'number' ? skill : skill.id;
+    const url = `${this.serverUrl}/${id}`;
+    return this.http.put<Skill>(url, skill).pipe(
+      catchError(this.handleError<Skill>('updateSkill'))
+    );
+  }
+
+  deleteSkill(skill: Skill | number): Observable<Skill> {
+    const id = typeof skill === 'number' ? skill : skill.id;
+    const url = `${this.serverUrl}/${id}`;
+    return this.http.delete<Skill>(url).pipe(
+      catchError(this.handleError<Skill>('deleteSkill'))
+    );
+  }
+
+  searchSkill(valueSearch: string | number): Observable<Skill[]> {
+    return this.http.get<Skill[]>(`${this.serverUrl}/categories?idCategories=${valueSearch}`).pipe(
+      catchError(this.handleError<Skill[]>('searchSkill'))
     );
   }
 }
