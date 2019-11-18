3c51bf8391157295acf102d03887bebb08345e2a
thanhhuyen1208 <lunachris1208@gmail.com>
2019-11-18 16:51:04 +0700

    fix login page when after logged

---
+++ src/app/core/guard/auth-guard.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {AuthService} from '../services/auth.service';
+import {CanActivate, CanLoad, Router} from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuardService implements CanActivate, CanLoad {
+
+  constructor(
+    public auth: AuthService,
+    public router: Router
+  ) {
+  }
+
+  canActivate(): boolean {
+    if (!this.auth.isAuthenticated()) {
+      this.router.navigate(['login']);
+      return false;
+    }
+    return true;
+  }
+
+  canLoad(): boolean {
+    return this.canActivate();
+  }
+}
