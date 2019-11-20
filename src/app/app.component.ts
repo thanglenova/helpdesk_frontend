3275bc980a68d08a289a7828d8e4cfe577e5a2b2
khanhnguyen1025 <khanhnguyen@novahub.vn>
2019-11-20 11:01:41 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,35 @@
-import {Component} from '@angular/core';
-import {AuthService} from './core/services/auth.service';
-import {Router} from '@angular/router';
+import { Component } from "@angular/core";
+import { AuthService } from "./core/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+  
   isCollapsed: boolean;
-  constructor(private authService: AuthService, private router: Router) {
-  }
+  adminLogged: boolean;
+  
+  constructor(private authService: AuthService, private router: Router) {}
 
   public isLogged(): boolean {
+    this.adminLogged = this.authService.isAdmin();
     return this.authService.isLoggedIn();
   }
 
   public logout(): void {
-    localStorage.removeItem('currentUser');
-    this.router.navigate(['/login']);
+    localStorage.removeItem("currentUser");
+    this.router.navigate(["/login"]);
   }
 
   public profile(): void {
     this.authService.getProfileCurrent().subscribe(
       data => {
-        this.router.navigate(['/profile/' + data.id]);
+        this.router.navigate(["/profile/" + data.id]);
       },
-      error => {
-      }
+      error => {}
     );
   }
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,26 +1,35 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MatNativeDateModule } from '@angular/material/core';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
-import { NzFormModule } from 'ng-zorro-antd/form';
-import { NzTableModule } from 'ng-zorro-antd/table';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import {TableModule} from 'primeng/table';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzSelectModule } from 'ng-zorro-antd/select';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NzBadgeModule } from 'ng-zorro-antd/badge';
-import { NzCollapseModule } from 'ng-zorro-antd/collapse';
-import { NzEmptyModule } from 'ng-zorro-antd/empty';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MatNativeDateModule } from "@angular/material/core";
+import { NzInputModule } from "ng-zorro-antd/input";
+import { NzDatePickerModule } from "ng-zorro-antd/date-picker";
+import { NzFormModule } from "ng-zorro-antd/form";
+import { NzTableModule } from "ng-zorro-antd/table";
+import { NzGridModule } from "ng-zorro-antd/grid";
+import { NzButtonModule } from "ng-zorro-antd/button";
+import { TableModule } from "primeng/table";
+import { NzDropDownModule } from "ng-zorro-antd/dropdown";
+import { NzModalModule } from "ng-zorro-antd/modal";
+import { NzSelectModule } from "ng-zorro-antd/select";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NzBadgeModule } from "ng-zorro-antd/badge";
+import { NzCollapseModule } from "ng-zorro-antd/collapse";
+import { NzEmptyModule } from "ng-zorro-antd/empty";
+import { NzDividerModule } from "ng-zorro-antd/divider";
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatNativeDateModule,FormsModule,NzFormModule,ReactiveFormsModule,NzBadgeModule,NzCollapseModule,NzEmptyModule],
+  imports: [
+    CommonModule,
+    MatNativeDateModule,
+    FormsModule,
+    NzFormModule,
+    ReactiveFormsModule,
+    NzBadgeModule,
+    NzCollapseModule,
+    NzEmptyModule
+  ],
   exports: [
-
     // Material
     NzModalModule,
     TableModule,
@@ -35,8 +44,8 @@ import { NzEmptyModule } from 'ng-zorro-antd/empty';
     NzBadgeModule,
     ReactiveFormsModule,
     NzCollapseModule,
-    NzEmptyModule
+    NzEmptyModule,
+    NzDividerModule
   ]
 })
-export class AppMaterialModule {
-}
+export class AppMaterialModule {}
