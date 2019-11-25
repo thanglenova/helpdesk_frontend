b918284ad6cf809cbf0ca741ac9b8b003483217c
khanhnguyen1025 <khanhnguyen@novahub.vn>
2019-11-25 09:54:09 +0700

    fix-bug-welcome-pahe-init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import { NoGuardService } from "./core/guard/no-guard.service";
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "/login" },
   {
-    path: "user-day-offs",
+    path: "welcome",
     loadChildren: () =>
       import("./pages/welcome/welcome.module").then(m => m.WelcomeModule),
     canLoad: [AuthGuardService]
@@ -19,66 +19,65 @@ const routes: Routes = [
     canLoad: [NoGuardService]
   },
   {
-    path: "admin-dayoffs",
-    loadChildren: () =>
-      import("./pages/dayoff/dayoff.module").then(m => m.DayoffModule)
-  },
-  {
-    path: "admin-dayofftypes",
-    loadChildren: () =>
-      import("./pages/dayofftype/dayofftype.module").then(
-        m => m.DayofftypeModule
-      )
-  },
-  {
-    path: "admin-categoríes",
-    loadChildren: () =>
-      import("./pages/category/category.module").then(m => m.CategoryModule)
-  },
-  {
-    path: "admin-skills",
-    loadChildren: () =>
-      import("./pages/skill/skill.module").then(m => m.SkillModule)
-  },
-  {
     path: "profile/:id",
     loadChildren: () =>
       import("./pages/profile/profile.module").then(m => m.ProfileModule)
   },
   {
-    path: "skills",
-    loadChildren: () =>
-      import("./pages/skill/skill.module").then(m => m.SkillModule),
-    canLoad: [AuthGuardService]
-  },
-  {
-    path: "admin-users",
-    loadChildren: () =>
-      import("./pages/management/magement.module").then(m => m.MagementModule)
-  },
-
-  {
-    path: "admin-requests",
-    loadChildren: () =>
-      import("./pages/admin-request/admin-request.module").then(
-        m => m.AdminRequestModule
-      ),
-    canActivate: [AdminGuard]
-  },
-  {
-    path: "request-types",
-    loadChildren: () =>
-      import("./pages/request-type/request-type.module").then(
-        m => m.RequestTypeModule
-      ),
-    canActivate: [AdminGuard]
-  },
-  {
-    path: "user-requests",
+    path: "requests",
     loadChildren: () =>
       import("./pages/request-user/request-user.module").then(
         m => m.RequestUserModule
       )
+  },
+  {
+    path: "admin",
+    canActivate: [AdminGuard],
+    children: [
+      {
+        path: "day-offs",
+        loadChildren: () =>
+          import("./pages/dayoff/dayoff.module").then(m => m.DayoffModule)
+      },
+      {
+        path: "day-off-types",
+        loadChildren: () =>
+          import("./pages/dayofftype/dayofftype.module").then(
+            m => m.DayofftypeModule
+          )
+      },
+      {
+        path: "categories",
+        loadChildren: () =>
+          import("./pages/category/category.module").then(m => m.CategoryModule)
+      },
+      {
+        path: "skills",
+        loadChildren: () =>
+          import("./pages/skill/skill.module").then(m => m.SkillModule)
+      },
+      {
+        path: "users",
+        loadChildren: () =>
+          import("./pages/management/magement.module").then(
+            m => m.MagementModule
+          )
+      },
+      {
+        path: "requests",
+        loadChildren: () =>
+          import("./pages/admin-request/admin-request.module").then(
+            m => m.AdminRequestModule
+          )
+      },
+      {
+        path: "request-types",
+        loadChildren: () =>
+          import("./pages/request-type/request-type.module").then(
+            m => m.RequestTypeModule
+          )
+      }
+    ]
   }
 ];
 
