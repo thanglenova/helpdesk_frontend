f88bda7d599577f1f8adc1ecbf79903a36c5fe49
khanhnguyen1025 <khanhnguyen@novahub.vn>
2019-11-28 17:31:40 +0700

    push again

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, NgZone, ElementRef } from "@angular/core";
-import { Router } from "@angular/router";
-import { AuthService } from "src/app/core/services/auth.service";
-import { AlertService } from "src/app/core/services/alert.service";
-import { TokenService } from "src/app/core/services/token.service";
-import { NzMessageService } from "ng-zorro-antd/message";
+import { Component, OnInit, NgZone, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/core/services/auth.service';
+import { AlertService } from 'src/app/core/services/alert.service';
+import { TokenService } from 'src/app/core/services/token.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 
 declare const gapi: any;
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   submitted = false;
@@ -18,17 +18,17 @@ export class LoginComponent implements OnInit {
   returnUrl: string;
 
   clientId: string =
-    "10558520426-5epndmc1a1dgsjvffftbvn60rr6521hh.apps.googleusercontent.com";
+    '10558520426-5epndmc1a1dgsjvffftbvn60rr6521hh.apps.googleusercontent.com';
   loginError: string;
   error: {};
 
   private scope = [
-    "profile",
-    "email",
-    "https://www.googleapis.com/auth/plus.me",
-    "https://www.googleapis.com/auth/contacts.readonly",
-    "https://www.googleapis.com/auth/admin.directory.user.readonly"
-  ].join(" ");
+    'profile',
+    'email',
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/contacts.readonly',
+    'https://www.googleapis.com/auth/admin.directory.user.readonly'
+  ].join(' ');
 
   constructor(
     private token: TokenService,
@@ -41,7 +41,7 @@ export class LoginComponent implements OnInit {
   ) {
     //redirect to home if logged
     if (this.authService.currentUser) {
-      this.router.navigate["/welcome"];
+      this.router.navigate['/welcome'];
     }
   }
 
@@ -52,31 +52,34 @@ export class LoginComponent implements OnInit {
   public auth2: any;
 
   public googleInit() {
-    gapi.load("auth2", () => {
+    gapi.load('auth2', () => {
       this.auth2 = gapi.auth2.init({
         clientId:
-          "10558520426-5epndmc1a1dgsjvffftbvn60rr6521hh.apps.googleusercontent.com",
-        cookiepolicy: "single_host_origin",
-        scope: "profile email"
+          '10558520426-5epndmc1a1dgsjvffftbvn60rr6521hh.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
       });
-      this.attachSigin(document.getElementById("google-button"));
+      this.attachSigin(document.getElementById('google-button'));
     });
   }
 
   public attachSigin(element) {
     this.auth2.attachClickHandler(element, {}, googleUser => {
       let profile = googleUser.getBasicProfile();
-      console.log("Token|| " + googleUser.getAuthResponse().id_token);
       this.authService
         .loginGoogle(googleUser.getAuthResponse().id_token)
         .subscribe(
           data => {
-            this.ngZone.run(() => this.router.navigate(["/welcome"])).then();
+            this.ngZone.run(() => this.router.navigate(['/welcome'])).then();
             let token = data.accessToken;
-            localStorage.setItem("currentUser", token);
+            localStorage.setItem('currentUser', token);
           },
           error => {
-            this.message.error(`[${error.status}] ${error.error}`);
+            if (error.status === 406) {
+              this.message.error('Email is not Novahub email');
+            } else {
+              this.message.error(`${error.error}`);
+            }
           }
         );
     });
